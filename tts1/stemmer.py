031f162037be431ccad3a6184917404a2215a5d7
Mark Nemec <mrknmc@me.com>
2014-10-05 23:23:16 +0100

    making stemmer independent of stat measure

--- tts1/stemmer.py
+++ tts1/stemmer.py
@@ -2,28 +2,28 @@
 """
 
 import re
-import math
 
 from Stemmer import Stemmer
 from collections import defaultdict
-from common import read_std_files, dictify, log, memoize
+from tfidf import tfidf
+from common import read_std_files, dictify, log
 
 OUT_FILE = 'stemmer.top'
 TUNE_K = 5
-STEMMER = Stemmer('english')
+STEMMER = Stemmer('porter')
 
 
 def map_docs(docs_file):
     """Return total count of documents, tokens and inverted index counts."""
-    word_map = defaultdict(set)
+    word_map = defaultdict(int)
     doc_count = 0
     token_count = 0
 
-    for doc_id, doc_tokens in tokenize(docs_file):
+    for _, doc_tokens in tokenize(docs_file):
         doc_count += 1  # count document
         token_count += len(doc_tokens)  # count tokens
-        for token in doc_tokens:
-            word_map[token].add(doc_id)  # add doc_id to set for every word
+        for token in set(doc_tokens):
+            word_map[token] += 1  # increase inverted index count
 
     docs_file.seek(0)  # reset file pointer
     return doc_count, token_count, word_map
@@ -36,57 +36,11 @@ def tokenize(file_):
         line_txt = line_txt.strip().lower()
         line_tokens = re.split(r'\W+', line_txt)  # split on non-word chars
         if line_tokens[-1] == '':
-            line_tokens = line_tokens[:-1]  # drop the last word if ""
+            line_tokens = line_tokens[:-1]
         stemmed_tokens = STEMMER.stemWords(line_tokens)
         yield line_id, stemmed_tokens
 
     file_.seek(0)  # reset file pointer
-
-
-@memoize
-def emim(word1, word2, word_map=None, doc_count=None):
-    doc_set1, doc_set2 = word_map[word1], word_map[word2]
-    n_a, n_b = len(doc_set1), len(doc_set2)
-    n_ab = len(doc_set1 & doc_set2)
-    if n_a == 0 or n_b == 0:
-        return 0
-    pre_log = doc_count * (float(n_ab) / (n_a * n_b))
-    if pre_log == 0:
-        return 0
-    return n_ab * math.log(pre_log)
-
-
-@memoize
-def dice(word1, word2, word_map=None):
-    doc_set1, doc_set2 = word_map[word1], word_map[word2]
-    n_a, n_b = len(doc_set1), len(doc_set2)
-    n_ab = len(doc_set1 & doc_set2)
-    if n_a == 0 or n_b == 0 or n_ab == 0:
-        return 0
-    return float(n_ab) / (n_a + n_b)
-
-
-@memoize
-def chi_squared(word1, word2, word_map=None, doc_count=None):
-    doc_set1, doc_set2 = word_map[word1], word_map[word2]
-    n_a, n_b = len(doc_set1), len(doc_set2)
-    n_ab = len(doc_set1 & doc_set2)
-    if n_a == 0 or n_b == 0 or n_ab == 0:
-        return 0
-    return math.pow(float(n_ab) - (1.0 / doc_count) * n_a * n_b, 2) / (n_a * n_b)
-
-
-def sim(query_dct, doc_dct, doc_len, doc_count, avg_doc_len, word_map, k=TUNE_K):
-    """Computes tf.idf for a given query and document."""
-    emim_sum = 0.0
-    for q_word, tf_wq in query_dct.iteritems():
-        for d_word, tf_wd in doc_dct.iteritems():
-            tf = tf_wd / (tf_wd + ((k * doc_len) / avg_doc_len))
-            idf = math.log(doc_count / float(len(word_map[d_word])))
-            emim_val = chi_squared(q_word, d_word, word_map=word_map, doc_count=doc_count)
-            emim_sum += tf_wq * tf * idf * emim_val
-
-    return emim_sum
 
 
 def main():
@@ -95,12 +49,11 @@ def main():
         doc_count, token_count, word_map = map_docs(docs_file)
         avg_doc_len = token_count / float(doc_count)
         for doc_id, doc_tokens in tokenize(docs_file):
-            print(doc_id)
             doc_len = len(doc_tokens)
             doc_dct = dictify(doc_tokens)
             for query_id, query_tokens in tokenize(qrys_file):
                 query_dct = dictify(query_tokens)
-                similarity = sim(query_dct, doc_dct, doc_len, doc_count, avg_doc_len, word_map, k=TUNE_K)
+                similarity = tfidf(query_dct, doc_dct, doc_len, doc_count, avg_doc_len, word_map, k=TUNE_K)
                 log(out_file, query_id, doc_id, similarity)
 
 if __name__ == '__main__':
