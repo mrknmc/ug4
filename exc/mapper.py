9860b3ed8d26e70df694cd0101f920c17ad42989
Mark Nemec <mrknmc@me.com>
2014-10-19 23:52:40 +0100

    lol lowercase

--- exc/mapper.py
+++ exc/mapper.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python
 
 """
-hadoop jar contrib/streaming/hadoop-0.20.2-streaming.jar \
+hadoop jar /opt/hadoop/hadoop-0.20.2/contrib/streaming/hadoop-0.20.2-streaming.jar \
     -input /user/s1250553/ex1/webSmall.txt \
     -output /user/s1140740/ex1/output \
     -mapper mapper.py \
     -file mapper.py
+    -reducer cat \
 """
 
 import sys
 
 for line in sys.stdin:
-    print(line.lowercase())
+    print(line.lower())
