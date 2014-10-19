05076140c8383003f5a565ae02e54bcd149066c4
Mark Nemec <mrknmc@me.com>
2014-10-19 19:00:21 +0100

    refactored brute to be hopefully more efficient

--- tts2/brute.py
+++ tts2/brute.py
@@ -4,47 +4,39 @@ DEFAULT_IDF = 13.6332
 
 
 class Story(object):
-    def __init__(self, id, vec):
+    def __init__(self, id, vec, idfs):
         self.id, self.vec = id, vec
+        tfidf_sum = 0.0
+        for word, tf_wq in vec.iteritems():
+            idf = idfs.get(word, DEFAULT_IDF)
+            tfidf_sum += tf_wq * tf_wq * idf * idf
+        self.tfidf = tfidf_sum
 
 
 def similarity(story1, story2, idfs):
     """Computes the similarity of two stories using cosines."""
     qw_dw = tfidf(story1, story2, idfs)
-    qw_qw = tfidf(story1, story1, idfs)
-    dw_dw = tfidf(story2, story2, idfs)
+    qw_qw = story1.tfidf
+    dw_dw = story2.tfidf
     return qw_dw / pow(qw_qw * dw_dw, 0.5)
 
 
 def tfidf(story1, story2, idfs):
     """Computes tf.idf for a given query and document."""
-    # create cache if not there
-    if not hasattr(tfidf, 'cache'):
-        tfidf.cache = {}
-    cache = tfidf.cache
-
-    # use value from cache if available
-    ids = story1.id, story2.id
-    if ids in cache:
-        return cache[ids]
-
-    # otherwise compute tfidf
     tfidf_sum = 0.0
     for word, tf_wq in story1.vec.iteritems():
         tf_wd = story2.vec.get(word, 0)
-        tfidf_sum += tf_wq * tf_wd * pow(idfs.get(word, DEFAULT_IDF), 2)
-
-    if ids[0] == ids[1]:
-        cache[ids] = tfidf_sum
+        idf = idfs.get(word, DEFAULT_IDF)
+        tfidf_sum += tf_wq * tf_wd * idf * idf
     return tfidf_sum
 
 
-def parse_news(file_):
+def parse_news(file_, idfs):
     """Tokenize line into id and lowercase token vector."""
     for line in file_:
         story_id, line_txt = line.split(' ', 1)
         tokens = line_txt.strip().lower().split()
-        yield Story(id=int(story_id), vec=dictify(tokens))
+        yield Story(int(story_id), dictify(tokens), idfs)
 
 
 def parse_idfs(news_idf):
@@ -70,14 +62,14 @@ def dictify(tokens):
     return dct
 
 
-def main(thresh=0.2, stop=10000):
+def main(thresh=0.2, stop=1000):
     try:
         news_txt = open('news.txt')
         news_idf = open('news.idf')
         out_file = open('pairs.out', 'w')
 
-        stories = parse_news(news_txt)  # story generator
         idfs = parse_idfs(news_idf)  # create idf map
+        stories = parse_news(news_txt, idfs)  # story generator
         cache = [stories.next()]  # put first story in the cache
 
         # for every story starting from #2 and stopping at #10,000
@@ -88,15 +80,16 @@ def main(thresh=0.2, stop=10000):
             # compare to every story in the cache
             sims = ((sim(prev_story), prev_story) for prev_story in cache)
             # get story with max similarity
-            sim, max_story = max(sims, key=lambda x: x[0])
+            max_sim, max_story = max(sims, key=lambda x: x[0])
             # output ids if similarity above thresh
-            if sim > thresh:
+            if max_sim > thresh:
                 log(out_file, cur_story.id, max_story.id)
             # add the story to the cache after done
             cache.append(cur_story)
 
             if idx == stop:
                 break
+
     finally:
         news_txt.close()
         news_idf.close()
