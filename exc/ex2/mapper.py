ec8038964aa44276e78f029b68ceb0b363eb2846
Mark Nemec <mrknmc@me.com>
2014-10-20 00:46:34 +0100

    fixed last line missing

--- exc/ex2/mapper.py
+++ exc/ex2/mapper.py
@@ -2,7 +2,7 @@
 
 """
 hadoop jar /opt/hadoop/hadoop-0.20.2/contrib/streaming/hadoop-0.20.2-streaming.jar \
-    -input /user/s1250553/ex2/webSmall.txt \
+    -input /user/s1140740/ex2/input \
     -output /user/s1140740/ex2/output \
     -mapper mapper.py \
     -file mapper.py \
--- exc/ex2/reducer.py
+++ exc/ex2/reducer.py
@@ -2,9 +2,13 @@
 
 import sys
 
-prev_line = -1
+prev_line = None
 
 for line in sys.stdin:
     if line != prev_line:
-        print(prev_line)
+        if prev_line is not None:
+            print(prev_line)
         prev_line = line
+
+if prev_line == line:
+    print(line)
