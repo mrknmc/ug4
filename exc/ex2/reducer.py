310e1d1460141927219bcda2a10ed4d9b1314d0f
Mark Nemec <mrknmc@me.com>
2014-10-20 11:14:45 +0100

    stripping \n and removed mapper

--- exc/ex2/reducer.py
+++ exc/ex2/reducer.py
@@ -1,10 +1,20 @@
 #!/usr/bin/python
 
+"""
+hadoop jar /opt/hadoop/hadoop-0.20.2/contrib/streaming/hadoop-0.20.2-streaming.jar \
+    -input /user/s1140740/ex2/input \
+    -output /user/s1140740/ex2/output \
+    -mapper cat \
+    -reducer reducer.py \
+    -file reducer.py
+"""
+
 import sys
 
 prev_line = None
 
 for line in sys.stdin:
+    line = line.strip()
     if line != prev_line:
         if prev_line is not None:
             print(prev_line)
