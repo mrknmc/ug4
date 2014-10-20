45cd61f3855035415fc7d172e145bc67aa232dfd
Mark Nemec <mrknmc@me.com>
2014-10-20 18:06:44 +0100

    changed format to theirs for task4

--- exc/task4/mapper.py
+++ exc/task4/mapper.py
@@ -15,5 +15,6 @@ import sys
 for line in sys.stdin:
     words = line.strip().split()
     for triple in (words[i:i+3] for i in range(len(words) - 2)):
-        triple = ' '.join(triple)
+        triple = ('{0!r}'.format(t) for t in triple)
+        triple = ', '.join(triple)
         print('{0}\t{1}'.format(triple, 1))
