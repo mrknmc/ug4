d64981e0fb72a1ea71ef5ab6da38e3853e999322
Mark Nemec <mrknmc@me.com>
2014-10-20 23:14:32 +0100

    now printing out row no as well

--- exc/task7/reducer.py
+++ exc/task7/reducer.py
@@ -5,11 +5,13 @@ import sys
 prev_col_no = None
 
 for line in sys.stdin:
-    col_no_row_no, col_str = line.strip().split('\t')
-    col_no, row_no = col_no_row_no.split(',')
+    col_no, row_no, col_str = line.strip().split(',')
     # not first column and column changed -> print \n
     if prev_col_no is not None and col_no != prev_col_no:
-        print('')
+        # print('')
+        print('\n{0}\t'.format(col_no)),
+    elif prev_col_no is None:
+        print('{0}\t'.format(col_no)),
 
     print(col_str),
     prev_col_no = col_no
