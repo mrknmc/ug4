3177cbec9e0ea41d87184affff6aee29fcfa3ce5
Mark Nemec <mrknmc@me.com>
2014-10-20 20:02:50 +0100

    did task5

--- exc/task5/mapper.py
+++ exc/task5/mapper.py
@@ -12,9 +12,15 @@ hadoop jar /opt/hadoop/hadoop-0.20.2/contrib/streaming/hadoop-0.20.2-streaming.j
 
 import sys
 
+triples = {}
+
 for line in sys.stdin:
     words = line.strip().split()
     for triple in (words[i:i+3] for i in range(len(words) - 2)):
         triple = ('{0!r}'.format(t) for t in triple)
         triple = ', '.join(triple)
-        print('{0}\t{1}'.format(triple, 1))
+        triples.setdefault(triple, 0)
+        triples[triple] += 1
+
+for triple, count in triples.iteritems():
+    print('{0}\t{1}'.format(triple, count))
