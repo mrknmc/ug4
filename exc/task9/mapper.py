8359d249d2d5889cf8d4afce18ba0fbecfb8fcbd
Mark Nemec <mrknmc@me.com>
2014-10-25 14:07:59 +0100

    added awk script to task9

--- exc/task9/mapper.py
+++ exc/task9/mapper.py
@@ -2,12 +2,14 @@
 
 """
 hadoop jar /opt/hadoop/hadoop-0.20.2/contrib/streaming/hadoop-0.20.2-streaming.jar \
-    -input /user/s1250553/ex1/uniSmall.txt \
+    -input /user/s1250553/ex1/uniLarge.txt \
     -output /user/s1140740/task9/output \
     -mapper mapper.py \
     -file mapper.py \
     -reducer reducer.py \
     -file reducer.py
+
+hadoop dfs -cat /user/s1140740/task9/output/* | sort -nr | awk 'BEGIN{max=0}{if($1>=max){print $2; max=$1}}'
 """
 
 import sys
