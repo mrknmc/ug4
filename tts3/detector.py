52b7b7191b62afc59c35e294aa3def9a94f5a175
Mark Nemec <mrknmc@me.com>
2014-11-01 18:02:16 +0000

    doing task3

--- tts3/detector.py
+++ tts3/detector.py
@@ -1,13 +1,60 @@
+import math
+import hashlib
 import re
+
+from contextlib import nested
+from collections import defaultdict
+
+
+DEFAULT_K = 24
+STOP_WORDS = [word.rstrip('\n') for word in open('english.txt').readlines()]
+
+
+def dictify(tokens):
+    """Turn tokens into a dict with words as keys and counts as values."""
+    dct = {}
+    for token in tokens:
+        dct.setdefault(token, 0)
+        dct[token] += 1
+    return dct
+
+
+def hash_word(word):
+    """Hashes a word into a binary sequence."""
+    # get 16byte hash
+    digest = hashlib.md5(word).digest()
+    # convert to 4 byte chunks
+    chunks = (digest[4 * i:4 * (i + 1)].encode('hex') for i in range(len(digest) / 4))
+    # convert to  ints
+    int_chunks = (int(chunk, 16) for chunk in chunks)
+    # convert to one binary sequence
+    return ''.join(bin(chunk).lstrip('0b') for chunk in int_chunks)
+
+
+def hashsim(doc, k=DEFAULT_K):
+    """Hashes a document using simhash."""
+    # make array to store results
+    hash_sum = [0] * 128
+    for word, freq in doc.iteritems():
+        # compute hash for every word
+        word_hash = hash_word(word)
+        for idx, char in enumerate(word_hash):
+            # sum char vals in each column
+            hash_sum[idx] += freq if char == '1' else -freq
+    # combine into a binary string
+    fingerprint = ''.join('1' if hsum > 0 else '0' for hsum in hash_sum)
+    return (fingerprint[k * i:k * (i + 1)] for i in range(len(fingerprint) / k))
 
 
 def parse(file_):
-    """"""
+    """Parses the file, tokenizes and removes stop words."""
     for line in file_:
-        line = line.strip()
-        story_id, rest = line.split(' ', 1)
-        tokens = re.split(r'[\t\r\n\\~`!@#$%^&*()_-+=[]{}|:;"\'<>,.?/]+', rest)
-        yield story_id, tokens
+        story_id, rest = line.strip().lower().split(' ', 1)
+        # tokenize
+        tokens = re.split(r'[\t\r\n\\~`!@#$%^&*\(\)_\-+=\[\]\{\}|:;"\'<>,.?/]+', rest)
+        # remove stop words
+        tokens = [token for token in tokens if token not in STOP_WORDS]
+        yield story_id, dictify(tokens)
 
 
 def simple_parse(file_):
@@ -17,12 +64,25 @@ def simple_parse(file_):
         yield story_id, rest
 
 
+def cosine(doc1, doc2):
+    """Compute cosine similarity measure."""
+    dw_dw = sum(tf_wq * tf_wq for tf_wq in doc2.itervalues())
+    qw_qw = 0.0
+    qw_dw = 0.0
+    for word, tf_wq in doc1.iteritems():
+        qw_qw += tf_wq * tf_wq
+        if word in doc2:
+            qw_dw += tf_wq * doc2[word]
+
+    return qw_dw / math.sqrt(qw_qw * dw_dw)
+
+
 def comparator(story_id):
     """Returns the int value from a story id."""
     return int(story_id.lstrip('t'))
 
 
-def main():
+def main1():
     with open('data.train') as train:
         seen = {}
         for story_id, text in simple_parse(train):
@@ -34,5 +94,32 @@ def main():
                 seen[text] = story_id
 
 
+def main2():
+    with nested(
+        open('data.train'),
+        open('type1.dup'),
+        open('type2.dup'),
+    ) as (train, type1, type2):
+        buckets = [defaultdict(list) for i in range(5)]
+        for story_id, tokens in parse(train):
+            # compute the fingerprint hashes
+            hashes = hashsim(tokens)
+            similar = []
+            for idx, hash_ in enumerate(hashes):
+                bucket = buckets[idx]
+                if hash_ in bucket:
+                    # compare to everything in the same bucket
+                    for seen_story_id, seen_tokens in bucket[hash_]:
+                        sim = cosine(tokens, seen_tokens)
+                        if sim > 0.8 and 1.0:
+                            similar.append(seen_story_id)
+                else:
+                    bucket[hash_].append((story_id, tokens))
+
+            for see_story_id in set(similar):
+                orig, dup = sorted([seen_story_id, story_id], key=comparator)
+                print('{0} {1}'.format(orig, dup))
+
+
 if __name__ == '__main__':
-    main()
+    main2()
--- tts3/report.md
+++ tts3/report.md
@@ -11,15 +11,21 @@ The purpose of this report is to provide an overview of decisions made in design
 
 Type 1
 
-:   Plagiarisms of this type are exact duplicates, e.g. documents t104 and t4172 which are classified as type 1 are exactly the same, word for word.
+:   Plagiarisms of this type are exact duplicates, e.g. documents `t104` and `t4172` which are classified as type 1 are exactly the same, word for word.
 
 
 Type 2
 
-:   Plagiarisms of this type are near duplicates, e.g. documents t1088 and t5015 which are classified as type 2 are almost the same, except document t1088 is missing the word _room_.
+:   Plagiarisms of this type are near duplicates, e.g. documents `t1088` and `t5015` which are classified as type 2 are almost the same, except document `t1088` is missing the word _room_.
 
 
 # Type 1 detection
+
+Detecting plagiarisms of type 1 was easy. The content of each story is extracted and if it is the first time we encountered this content it is stored in a dictionary as a key with the story id as a value. However, if we have seen the exact same content before we flag the documents as duplicates.
+
+# Type 2 detection
+
+
 
 # Notes
 
