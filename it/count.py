5ed6ac86820b8f3c0e571b259bf98b3f74e597bb
Mark Nemec <mrknmc@me.com>
2014-11-12 15:31:57 +0000

    task3

--- it/count.py
+++ it/count.py
@@ -16,40 +16,73 @@ def entropy(dist):
     return sum(-prob * math.log(prob, 2) for prob in dist.values())
 
 
-def unigram(file_):
-    """Computes the bigram distribution of a file."""
-    counter = Counter(char_gen(file_))
-    chars = sum(counter.values())
-    return {key: (val / float(chars)) for key, val in counter.items()}
-
-
-def char_gen(file_):
+def unigen(file_):
     """Takes a file and turns it into a character generator, ignoring \n."""
     prog = re.compile(r'[a-z ]')
     return (c.lower() for c in chain(*file_) if prog.match(c))
 
 
-def bigram(file_):
-    """Computes the bigram distribution of a file."""
-    file1, file2 = [char_gen(file_gen) for file_gen in tee(file_)]
+def bigen(file_):
+    """Bigram generator from a file."""
+    file1, file2 = [unigen(file_gen) for file_gen in tee(file_)]
     next(file2)  # drop first char
-    counter = Counter(zip(file1, file2))
+    return zip(file1, file2)
+
+
+def unigram(file_):
+    """Computes the bigram distribution of a file."""
+    counter = Counter(unigen(file_))
     chars = sum(counter.values())
     return {key: (val / float(chars)) for key, val in counter.items()}
+
+
+def bigram(file_):
+    """Computes the bigram distribution of a file."""
+    counter = Counter(bigen(file_))
+    chars = sum(counter.values())
+    return {key: (val / float(chars)) for key, val in counter.items()}
+
+
+def iid_length(file_, dist):
+    """Length of a file if its chars were i.i.d."""
+    probs = (dist[c] for c in unigen(file_))
+    file_len = -sum(math.log(prob, 2) for prob in probs)
+    # turn into int
+    return file_len + 2
+
+
+def bi_length(file_, uni_dist, bi_dist):
+    """Length of a file if its chars were bigramy."""
+    chars = list(bigen(file_))
+    char1 = chars[0][0]
+    probs = [bi_dist[cs] / uni_dist[cs[0]] for cs in chars]
+    probs.append(uni_dist[char1])
+    file_len = - sum(math.log(prob, 2) for prob in probs)
+    # turn into int
+    return file_len + 2
 
 
 def main():
     """Do the thing."""
     with open(FILE) as file_:
-        file1, file2 = tee(file_)
-        uni_dist = unigram(file1)
+        uni_dist = unigram(file_)
         ent_x = entropy(uni_dist)
-        print('{0:.4}'.format(ent_x))
-        bi_dist = bigram(file2)
+        print('H(X_n): {0:.4}'.format(ent_x))
+
+        file_.seek(0)
+        bi_dist = bigram(file_)
         ent_joint = entropy(bi_dist)
-        print('{0:.4}'.format(ent_joint))
+        print('H(X_n, X_n+1): {0:.4}'.format(ent_joint))
         ent_cond = ent_joint - ent_x
-        print('{:.4}'.format(ent_cond))
+        print('H(X_n+1 | X_n): {:.4}'.format(ent_cond))
+
+        file_.seek(0)
+        iid_len = iid_length(file_, uni_dist)
+        print('i.i.d. length: {:.4}'.format(iid_len))
+
+        file_.seek(0)
+        bi_len = bi_length(file_, uni_dist, bi_dist)
+        print('bigram length: {:.4}'.format(bi_len))
 
 
 if __name__ == '__main__':
