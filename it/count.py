e8805f0f5274af52740b340c0132f4892886b505
Mark Nemec <mrknmc@me.com>
2014-11-12 16:08:24 +0000

    started task4

--- it/count.py
+++ it/count.py
@@ -47,8 +47,7 @@ def iid_length(file_, dist):
     """Length of a file if its chars were i.i.d."""
     probs = (dist[c] for c in unigen(file_))
     file_len = -sum(math.log(prob, 2) for prob in probs)
-    # turn into int
-    return file_len + 2
+    return int(math.ceil(file_len + 2))
 
 
 def bi_length(file_, uni_dist, bi_dist):
@@ -58,8 +57,37 @@ def bi_length(file_, uni_dist, bi_dist):
     probs = [bi_dist[cs] / uni_dist[cs[0]] for cs in chars]
     probs.append(uni_dist[char1])
     file_len = - sum(math.log(prob, 2) for prob in probs)
-    # turn into int
-    return file_len + 2
+    return int(math.ceil(file_len + 2))
+
+
+def iid_round_length(file_, dist, bits=8):
+    """"""
+    pow2 = pow(2, bits)
+    # rounding
+    round_dist = {k: math.ceil(pow2 * p) / pow2 for k, p in dist.items()}
+    # normalizing sum
+    round_sum = sum(p for p in round_dist.values())
+    # renormalizing
+    round_dist = {k: p / round_sum for k, p in round_dist.items()}
+    # each char needs bits in header
+    header = len(round_dist.keys()) * bits
+    data = iid_length(file_, dist)
+    return {'header': header, 'data': data, 'total': header + data}
+
+
+def bi_round_length(file_, uni_dist, bi_dist, bits=8):
+    """"""
+    pow2 = pow(2, bits)
+    # rounding
+    round_dist = {k: math.ceil(pow2 * p) / pow2 for k, p in dist.items()}
+    # normalizing sum
+    round_sum = sum(p for p in round_dist.values())
+    # renormalizing
+    round_dist = {k: p / round_sum for k, p in round_dist.items()}
+    # each char needs bits in header
+    header = len(round_dist.keys()) * bits
+    data = iid_length(file_, dist)
+    return {'header': header, 'data': data, 'total': header + data}
 
 
 def main():
@@ -78,12 +106,19 @@ def main():
 
         file_.seek(0)
         iid_len = iid_length(file_, uni_dist)
-        print('i.i.d. length: {:.4}'.format(iid_len))
+        print('i.i.d. length: {}'.format(iid_len))
 
         file_.seek(0)
         bi_len = bi_length(file_, uni_dist, bi_dist)
-        print('bigram length: {:.4}'.format(bi_len))
+        print('bigram length: {}'.format(bi_len))
 
+        file_.seek(0)
+        iid_round_len = iid_round_length(file_, uni_dist)
+        print('i.i.d. length rounded:\n\theader: {header}\n\tdata: {data}\n\ttotal: {total}'.format(**iid_round_len))
+
+        file_.seek(0)
+        bi_round_len = bi_round_length(file_, uni_dist)
+        print('i.i.d. length rounded:\n\theader: {header}\n\tdata: {data}\n\ttotal: {total}'.format(**bi_round_len))
 
 if __name__ == '__main__':
     main()
