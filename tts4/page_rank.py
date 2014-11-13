e03436602c777045a1bfa1a34a4e8f3c6594d8f1
Mark Nemec <mrknmc@me.com>
2014-11-13 19:46:05 +0000

    added HITS algorithm

--- tts4/page_rank.py
+++ tts4/page_rank.py
@@ -1,8 +1,11 @@
+import math
+
+from contextlib import nested
 from collections import defaultdict
 
 
 def parse(f):
-    """"""
+    """Parses input file and creates a graph representation."""
     ins = defaultdict(lambda: defaultdict(int))
     outs = defaultdict(lambda: defaultdict(int))
     people = set()
@@ -44,13 +47,61 @@ def page_rank(people, outs, ins, iters, lmb=0.8):
     return probs
 
 
+def normalize(dist):
+    """Normalizes probability distributions so squares add to 1."""
+    denom = math.sqrt(sum(prob * prob for prob in dist.values()))
+    return {p: prob / denom for p, prob in dist.items()}
+
+
+def hits(people, outs, ins, iters):
+    """HITS algorithm."""
+    denom = math.sqrt(len(people))
+    hubs = {person: 1.0 / denom for person in people}
+    auths = {person: 1.0 / denom for person in people}
+    new_hubs = {}
+    new_auths = {}
+
+    print(hubs['jeff.dasovich@enron.com'])
+    print(hubs['john.lavorato@enron.com'])
+    print(auths['jeff.dasovich@enron.com'])
+    print(auths['john.lavorato@enron.com'])
+
+    for i in range(iters):
+        for p in people:
+            new_hubs[p] = sum(w * auths[rec] for rec, w in outs[p].items())
+            new_auths[p] = sum(w * hubs[sen] for sen, w in ins[p].items())
+
+        hubs, new_hubs = normalize(new_hubs), hubs
+        auths, new_auths = normalize(new_auths), auths
+
+        if i == 0:
+            print(hubs['jeff.dasovich@enron.com'])
+            print(hubs['john.lavorato@enron.com'])
+            print(auths['jeff.dasovich@enron.com'])
+            print(auths['john.lavorato@enron.com'])
+    return hubs, auths
+
+
+def log(f, dist, n=100):
+    """Writes n emails with highest scores to a file."""
+    for (email, score) in sorted(dist.items(), key=lambda x: -x[1])[:n]:
+        f.write('{0:.6f} {1}\n'.format(score, email))
+
+
 def main():
     """"""
-    with open('graph.txt') as f:
+    with nested(
+        open('graph.txt'),
+        open('hubs.txt', 'w'),
+        open('auth.txt', 'w'),
+        open('pr.txt', 'w'),
+    ) as (f, h, a, pr):
         people, outs, ins = parse(f)
-        probs = page_rank(people, outs, ins, 10)
-        for tup in sorted(probs.items(), key=lambda x: -x[1])[:10]:
-            print(tup)
+        # pranks = page_rank(people, outs, ins, 10)
+        # log(pr, pranks)
+        hubs, auths = hits(people, outs, ins, 10)
+        log(h, hubs)
+        log(a, auths)
 
 
 if __name__ == '__main__':
