b68c7b7f1419e5b5708783cc14341d9ba577885c
Mark Nemec <mrknmc@me.com>
2014-11-15 19:39:00 +0000

    added graph.py

--- tts4/graph.py
+++ tts4/graph.py
@@ -1,4 +1,3 @@
-import math
 import re
 import networkx as nx
 import matplotlib
@@ -18,17 +17,11 @@ def main():
 
     # take first 10 peeps with highest PR
     with open('pr.txt') as pr:
-        total_rank = 0.0
         for idx, line in enumerate(pr):
             rank, email = line.split()
             if idx < 10:
                 rank = float(rank)
-                total_rank += rank
                 G.add_node(email, {'rank': rank, 'shape': 'box'})
-
-    # size is their PR / total
-    for (node, data) in G.nodes(data=True):
-        G.node[node]['ratio'] = data['rank'] / total_rank
 
     unames2emails = dict((node.split('@', 1)[0], node) for node in G.nodes())
 
@@ -48,6 +41,8 @@ def main():
                 if company is not None:
                     G.node[email]['label'] = '\n'.join([email, name, role, company])
 
+    reverse_emails = {}
+
     # create edges between people who sent emails
     with open('graph.txt') as graph:
         outs = defaultdict(lambda: defaultdict(set))
@@ -55,23 +50,21 @@ def main():
         for line in graph:
             email_id, sender, receiver = line.split()
             if sender in G and receiver in G and sender != receiver:
+                reverse_emails[email_id] = sender, receiver
                 outs[sender][receiver].add(email_id)
 
         for idx, (sender, data) in enumerate(outs.items(), start=1):
             for receiver, emails in data.items():
-                G.add_edge(sender, receiver)
+                # take a log to scale
+                G.add_edge(
+                    sender, receiver,
+                    taillabel=len(emails),
+                    color='gray30',
+                    fontcolor='crimson',
+                    labelfontsize=18
+                )
 
     nx.write_dot(G, 'graph.dot')
-    # G = nx.convert_node_labels_to_integers(G)
-
-    # print(nx.get_node_attributes(G, 'ratio').values())
-
-    # nx.draw_circular(
-    #     G,
-    #     node_size=[size * 100000 for size in nx.get_node_attributes(G, 'ratio').values()],
-    #     # with_labels=True
-    # )
-    # plt.show()
 
 if __name__ == '__main__':
     main()
