a9bab92bed19282e3f2dbe3580cae0f2848ed15b
Mark Nemec <mrknmc@me.com>
2014-11-19 23:13:29 +0000

    added flipper and added bit error tests

--- it/test.py
+++ it/test.py
@@ -1,8 +1,20 @@
 import unittest
-import math
 
 from count import *
+from flipper import *
 from code import *
+
+
+class MockStdIn(object):
+    """Mock of stdin to test Encoder and Decoder."""
+
+    def __init__(self, string):
+        self.string = string + '\n'
+
+    def read(self, n):
+        ret = self.string[0]
+        self.string = self.string[1:]
+        return ret
 
 
 class TestSourceCoding(unittest.TestCase):
@@ -68,34 +80,22 @@ class TestSourceCoding(unittest.TestCase):
             'total': 237,
         })
 
-    # def test_bi_round_length(self):
-    #     """Test rounding scheme using bigrams works."""
-    #     a = ' abcdabcaba'
-    #     uni_dist = unigram(a)
-    #     bi_dist = bigram(a)
-    #     self.assertEqual(bi_round_length(a, uni_dist, bi_dist), {
-    #         'header': 26 * 8,  # 5x5 chars + first char, 8 bits each
-    #         'data': 11,
-    #         'total': 211,
-    #     })
-
-
-class MockStdIn(object):
-    """"""
-
-    def __init__(self, string):
-        self.string = string + '\n'
-
-    def read(self, n):
-        ret = self.string[0]
-        self.string = self.string[1:]
-        return ret
+    def test_bi_round_length(self):
+        """Test rounding scheme using bigrams works."""
+        a = ' abcdabcaba'
+        uni_dist = unigram(a)
+        bi_dist = bigram(a)
+        self.assertEqual(bi_round_length(a, uni_dist, bi_dist), {
+            'header': 6048,  # 27 * 27 + 27 chars, 8 bits each
+            'data': 11,
+            'total': 211,
+        })
 
 
 class TestNoisyChannel(unittest.TestCase):
 
     def test_nutritious_snacks(self):
-        """"""
+        """Test nutritious_snacks function."""
         result = nutritious_snacks('abcd', [97, 2, 6, 21])
         self.assertEqual(result, '\x00`eq')
 
@@ -124,10 +124,108 @@ class TestNoisyChannel(unittest.TestCase):
         self.assertEqual(result, '0100')
 
     def test_decode_two_errors_raises(self):
-        """Test that code with two errors raises Exception."""
+        """Test that code with two errors returns errors."""
         stream = MockStdIn('11110001')
-        with self.assertRaises(Exception):
-            ''.join(decode(stream))
+        result = ''.join(decode(stream))
+        self.assertEqual(result, '1110')
+
+
+class TestBitError(unittest.TestCase):
+
+    def block_error(self, f):
+        """Counts the number of block errors."""
+        success = failure = 0.
+        total = 100000
+        for i in range(total):
+            number = bin(int(round(random.random() * 16))).lstrip('0b').zfill(4)
+            stream = MockStdIn(number)
+            encoded = ''.join(encode(stream))
+            noised = ''.join(noise(MockStdIn(encoded), f=f))
+            try:
+                ''.join(decode(MockStdIn(noised)))
+                success += 1
+            except Exception:
+                failure += 1
+        return success / total, failure / total
+
+    def bit_error(self, f):
+        """Counts the average number of bit errors."""
+        success = failure = 0.
+        total = 0
+        for i in range(100000):
+            number = bin(int(round(random.random() * 16))).lstrip('0b').zfill(4)
+            stream = MockStdIn(number)
+            encoded = ''.join(encode(stream))
+            noised = ''.join(noise(MockStdIn(encoded), f=f))
+            decoded = ''.join(decode(MockStdIn(noised)))
+            total += len(number)
+            for enc, dec in zip(number, decoded):
+                if enc == dec:
+                    success += 1
+                else:
+                    failure += 1
+        return success / total, failure / total
+
+    def test_whatever(self):
+        total_failure = 0.
+        number = bin(int(round(random.random() * 16))).lstrip('0b').zfill(4)
+        stream = MockStdIn(number)
+        encoded = ''.join(encode(stream))
+        enc_len = len(encoded)
+        flippy = [()]
+        for i in range(enc_len):
+            flippy.append((i, ))
+            for j in range(i + 1, enc_len):
+                flippy.append((i, j))
+                for k in range(j + 1, enc_len):
+                    flippy.append((i, j, k))
+                    for l in range(k + 1, enc_len):
+                        flippy.append((i, j, k, l))
+                        for m in range(l + 1, enc_len):
+                            flippy.append((i, j, k, l, m))
+                            for n in range(m + 1, enc_len):
+                                flippy.append((i, j, k, l, m, n))
+                                for o in range(n + 1, enc_len):
+                                    flippy.append((i, j, k, l, m, n, o))
+                                    for p in range(o + 1, enc_len):
+                                        flippy.append((i, j, k, l, m, n, o, p))
+
+        for flip in flippy:
+            copy = [int(c) for c in encoded]
+            prob = pow(0.001, len(flip)) * pow(0.999, 8 - len(flip))
+            for f in flip:
+                copy[f] = 1 - copy[f]  # flip it
+            noised = ''.join(str(c) for c in copy)
+            decoded = ''.join(decode(MockStdIn(noised)))
+            failure = 0.
+            for enc, dec in zip(number, decoded):
+                if enc != dec:
+                    failure += 1
+            total_failure += prob * (failure / 4)
+        print(total_failure)
+        self.assertTrue(False)
+
+    @unittest.skip
+    def test_04(self):
+        """Test with 0.4 probability."""
+        successes, failures = self.bit_error(0.4)
+        msg = '\nsuccesses: {}\nfailures: {}'.format(successes, failures)
+        self.assertTrue(False, msg=msg)
+
+    @unittest.skip
+    def test_01(self):
+        """Test with 0.1 probability."""
+        successes, failures = self.bit_error(0.1)
+        msg = '\nsuccesses: {}\nfailures: {}'.format(successes, failures)
+        self.assertTrue(False, msg=msg)
+
+    @unittest.skip
+    def test_0001(self):
+        """Test with 0.001 probability."""
+        successes, failures = self.bit_error(0.001)
+        msg = '\nsuccesses: {}\nfailures: {}'.format(successes, failures)
+        self.assertTrue(False, msg=msg)
+
 
 if __name__ == '__main__':
     unittest.main()
