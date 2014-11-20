1505d8baaf3d097add8ff34abfded7ba0f6d2764
Mark Nemec <mrknmc@me.com>
2014-11-20 01:24:48 +0000

    refactored code for bit error tests

--- it/test.py
+++ it/test.py
@@ -1,8 +1,15 @@
 import unittest
 
+from itertools import chain, combinations
 from count import *
 from flipper import *
 from code import *
+
+
+def generate_num(n):
+    """Generates a random n-bit binary number."""
+    pow2 = pow(2, n)
+    return bin(int(round(random.random() * pow2))).lstrip('0b').zfill(n)
 
 
 class MockStdIn(object):
@@ -132,32 +139,11 @@ class TestNoisyChannel(unittest.TestCase):
 
 class TestBitError(unittest.TestCase):
 
-    def generate_num(self, n):
-        """Generates a random n-bit binary number."""
-        pow2 = pow(2, n)
-        return bin(int(round(random.random() * pow2))).lstrip('0b').zfill(n)
-
-    def block_error(self, f):
-        """Counts the number of block errors."""
-        success = failure = 0.
-        total = 100000
-        for i in range(total):
-            number = generate_num(4)
-            stream = MockStdIn(number)
-            encoded = ''.join(encode(stream))
-            noised = ''.join(noise(MockStdIn(encoded), f=f))
-            decoded = ''.join(decode(MockStdIn(noised)))
-            if decoded == number:
-                success += 1
-            else:
-                failure += 1
-        return success / total, failure / total
-
-    def bit_error(self, f):
-        """Counts the average number of bit errors."""
-        success = failure = 0.
+    def analytic_bit_error(self, f, k):
+        """Counts the average number of bit errors in k transmissions."""
+        failure = 0.
         total = 0
-        for i in range(100000):
+        for i in range(k):
             number = generate_num(4)
             stream = MockStdIn(number)
             encoded = ''.join(encode(stream))
@@ -165,42 +151,24 @@ class TestBitError(unittest.TestCase):
             decoded = ''.join(decode(MockStdIn(noised)))
             total += len(number)
             for enc, dec in zip(number, decoded):
-                if enc == dec:
-                    success += 1
-                else:
+                if enc != dec:
                     failure += 1
-        return success / total, failure / total
+        return failure / total
 
-    def bit_error_prob(self):
+    def real_bit_error(self, f):
         """Computes probability of bit error."""
         total_failure = 0.
         number = generate_num(4)
         stream = MockStdIn(number)
         encoded = ''.join(encode(stream))
         enc_len = len(encoded)
-        flippy = [()]
-        for i in range(enc_len):
-            flippy.append((i, ))
-            for j in range(i + 1, enc_len):
-                flippy.append((i, j))
-                for k in range(j + 1, enc_len):
-                    flippy.append((i, j, k))
-                    for l in range(k + 1, enc_len):
-                        flippy.append((i, j, k, l))
-                        for m in range(l + 1, enc_len):
-                            flippy.append((i, j, k, l, m))
-                            for n in range(m + 1, enc_len):
-                                flippy.append((i, j, k, l, m, n))
-                                for o in range(n + 1, enc_len):
-                                    flippy.append((i, j, k, l, m, n, o))
-                                    for p in range(o + 1, enc_len):
-                                        flippy.append((i, j, k, l, m, n, o, p))
+        flippy = chain(*(combinations(range(8), i) for i in range(enc_len + 1)))
 
         for flip in flippy:
             copy = [int(c) for c in encoded]
-            prob = pow(0.001, len(flip)) * pow(0.999, 8 - len(flip))
-            for f in flip:
-                copy[f] = 1 - copy[f]  # flip it
+            prob = pow(f, len(flip)) * pow(1 - f, 8 - len(flip))
+            for pos in flip:
+                copy[pos] = 1 - copy[pos]  # flip it
             noised = ''.join(str(c) for c in copy)
             decoded = ''.join(decode(MockStdIn(noised)))
             failure = 0.
@@ -208,29 +176,25 @@ class TestBitError(unittest.TestCase):
                 if enc != dec:
                     failure += 1
             total_failure += prob * (failure / 4)
-        print(total_failure)
-        self.assertTrue(False)
+        return total_failure
 
-    @unittest.skip
-    def test_04(self):
+    def analytic_bit_error_test_04(self):
         """Test with 0.4 probability."""
-        successes, failures = self.bit_error(0.4)
-        msg = '\nsuccesses: {}\nfailures: {}'.format(successes, failures)
-        self.assertTrue(False, msg=msg)
+        real_failure_rate = self.real_bit_error(0.4)
+        failure_rate = self.analytic_bit_error(0.4, 10000)
+        self.assertAlmostEqual(real_failure_rate, failure_rate, places=1)
 
-    @unittest.skip
-    def test_01(self):
+    def analytic_bit_error_test_01(self):
         """Test with 0.1 probability."""
-        successes, failures = self.bit_error(0.1)
-        msg = '\nsuccesses: {}\nfailures: {}'.format(successes, failures)
-        self.assertTrue(False, msg=msg)
+        real_failure_rate = self.real_bit_error(0.1)
+        failure_rate = self.analytic_bit_error(0.1, 10000)
+        self.assertAlmostEqual(real_failure_rate, failure_rate, places=2)
 
-    @unittest.skip
-    def test_0001(self):
+    def analytic_bit_error_test_0001(self):
         """Test with 0.001 probability."""
-        successes, failures = self.bit_error(0.001)
-        msg = '\nsuccesses: {}\nfailures: {}'.format(successes, failures)
-        self.assertTrue(False, msg=msg)
+        real_failure_rate = self.real_bit_error(0.001)
+        failure_rate = self.analytic_bit_error(0.001, 100000)
+        self.assertAlmostEqual(real_failure_rate, failure_rate, places=5)
 
 
 if __name__ == '__main__':
