cd1330419370c50a820de7101791ff566a3fe7b7
Mark Nemec <mrknmc@me.com>
2014-12-02 13:02:29 +0000

    starting exc2
    --allow-empty

---
+++ exc2/task1/mapper.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python2.7
+
+import sys
+import os
+
+from collections import Counter
+
+INPUT_FILE = os.getenv('map_input_file')
+
+
+for line in sys.stdin:
+    tokens = line.strip().split()
+    counter = Counter(tokens)
+    for token, count in counter.iteritems():
+        print('{0} {1}\t{2}'.format(token, INPUT_FILE, count))
+
---
+++ exc2/task1/reducer.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python2.7
+
+import sys
+
+from collections import Counter
+
+
+def parse(stream):
+    counts = Counter()
+    prev_word = None
+    for line in stream:
+        keys, count = line.split('\t', 1)
+        word, doc = keys.split(' ', 1)
+        if word == prev_word:
+            counts[doc] += int(count)
+        else:
+            # word change, yield previous word
+            if prev_word is not None:
+                yield prev_word, counts
+                counts.clear()
+            prev_word = word
+            counts[doc] += int(count)
+    if counts:
+        yield word, counts
+
+
+for word, counts in parse(sys.stdin):
+
+    print('{0}:\t{1}\t{2}'.format(word, len(counts), sorted(counts.items())))
+
