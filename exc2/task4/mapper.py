938a2f8a35cadea2f50563279cf683def309cfa7
Mark Nemec <mrknmc@me.com>
2014-12-03 20:05:21 +0000

    started task4

---
+++ exc2/task4/mapper.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+import sys
+import re
+
+from itertools import groupby
+from operator import itemgetter
+
+
+def parse(stream):
+    for line in stream:
+        match = re.match(r'^\s*(.*?)\s+- -\s+?\[(.*?)\]\s*?"([A-Z]+)\s+?(.*?)\s+?(.+?)"\s+(\d+)\s+(\d+|-)\s*$', line)
+        if match is not None:
+            groups = match.groups()
+            status = groups[5]
+            if status == '404':
+                yield groups
+
+
+for host, reqs in groupby(parse(sys.stdin), key=itemgetter(0)):
+    print('{0}\t{1}'.format(url, len(list(reqs)))) 
