101ec14836efce5ce8bfb64f8a1719d7686d4317
Mark Nemec <mrknmc@me.com>
2014-12-03 20:08:13 +0000

    renamed task4 to avoid ambiguity

---
+++ exc2/task3.3/mapper.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+import sys
+import re
+
+from itertools import groupby
+from operator import itemgetter
+
+
+def parse(stream):
+    for line in stream:
+        match = re.match(r'^\s*(.*?)\s+- -\s+?\[(.*?)\]\s*?"([A-Z]+)\s+?(.*?)\s+?(.+?)"\s+(\d+)\s+(\d+|-)\s*$', line)
+        if match is not None:
+            yield match.groups()
+
+
+for url, reqs in groupby(parse(sys.stdin), key=itemgetter(3)):
+    print('{0}\t{1}'.format(url, len(list(reqs)))) 
