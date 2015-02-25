ff99f3f4be09427e9cee76bdb143f2994c4e26a3
Mark Nemec <mrknmc@me.com>
2015-02-25 14:50:38 +0000

    adding mesi and msi tests

--- pa2/test.py
+++ pa2/test.py
@@ -1,63 +1,70 @@
-import math
 import unittest
 
-from cache import coherence, make_instruction
+from cache import coherence
 
 
 DEFAULT_LINES = 1024
 DEFAULT_WORDS = 4
 
 
+INSTRS = [
+    'P0 R 20',
+    'P0 R 20',
+    'P0 R 10',
+    'P0 R 10',
+    'P1 W 10',
+    'P1 R 10',
+    'P1 W 30',
+    'P1 W 30',
+    'P0 R 30',
+    'P1 R 30',
+    'P0 R 30',
+]
+MSI_STATES = [
+    (0, 20, 'S'),  # miss
+    (0, 20, 'S'),  # hit (in Shared/Exclusive)
+    (0, 10, 'S'),  # miss
+    (0, 10, 'S'),  # hit (in Shared/Exclusive)
+    (1, 10, 'M'),  # miss
+    (1, 10, 'M'),  # hit (in Modified)
+    (1, 30, 'M'),  # miss
+    (1, 30, 'M'),  # hit (in Modified)
+    (0, 30, 'S'),  # miss
+    (1, 30, 'S'),  # hit (in Shared)
+    (0, 30, 'S'),  # hit (in Shared)
+]
+
+MESI_STATES = [
+    (0, 20, 'E'),  # miss
+    (0, 20, 'E'),  # hit (in Shared/Exclusive)
+    (0, 10, 'E'),  # miss
+    (0, 10, 'E'),  # hit (in Shared/Exclusive)
+    (1, 10, 'M'),  # miss
+    (1, 10, 'M'),  # hit (in Modified)
+    (1, 30, 'M'),  # miss
+    (1, 30, 'M'),  # hit (in Modified)
+    (0, 30, 'S'),  # miss
+    (1, 30, 'S'),  # hit (in Shared)
+    (0, 30, 'S'),  # hit (in Shared)
+]
+
+
 class Test(unittest.TestCase):
     """"""
 
-    instrs = [
-        'P0 R 20',
-        'P0 R 20',
-        'P0 R 10',
-        'P0 R 10',
-        'P1 W 10',
-        'P1 R 10',
-        'P1 W 30',
-        'P1 W 30',
-        'P0 R 30',
-        'P1 R 30',
-        'P0 R 30',
-    ]
-
-    states = [
-        (0, 20, 'S'),  # miss
-        (0, 20, 'S'),  # hit (in Shared/Exclusive)
-        (0, 10, 'S'),  # miss
-        (0, 10, 'S'),  # hit (in Shared/Exclusive)
-        (1, 10, 'M'),  # miss
-        (1, 10, 'M'),  # hit (in Modified)
-        (1, 30, 'M'),  # miss
-        (1, 30, 'M'),  # hit (in Modified)
-        (0, 30, 'S'),  # miss
-        (1, 30, 'S'),  # hit (in Shared)
-        (0, 30, 'S'),  # hit (in Shared)
-    ]
-
-    def test_make_instruction(self):
-        lines = 1024
-        index_size = int(math.log(lines, 2))
-        line = 'P1 W 5379'
-        inst = make_instruction(line, index_size)
-
-        rem = 5379 % 1024
-        self.assertEqual(rem, inst.index)
-        print(5379 / 1024.0)
-        print(inst.tag, inst.index)
-        self.assertFalse(True)
-
-    def test_instrs(self):
-        for caches, state in zip(coherence(self.instrs, DEFAULT_LINES, DEFAULT_WORDS), self.states):
+    def instructions(self, instrs, states, mesi):
+        gen = coherence(instrs, DEFAULT_LINES, DEFAULT_WORDS, mesi)
+        for caches, state in zip(gen, states):
             cache_id, addr, state = state
             index = addr % DEFAULT_LINES
             cache = caches[cache_id]
-            # check the state
             self.assertEqual(cache[index]['state'], state)
+
+    def test_msi_instructions(self):
+        self.instructions(INSTRS, MSI_STATES, False)
+
+    def test_mesi_instructions(self):
+        self.instructions(INSTRS, MESI_STATES, True)
 
 
 if __name__ == '__main__':
