c42712f367ae6c5aac4fe395bb4485f31a92fb61
Mark Nemec <mrknmc@me.com>
2015-03-07 22:22:09 +0000

    did partA

--- ppls2/aquadPartA.c
+++ ppls2/aquadPartA.c
@@ -63,9 +63,82 @@ int main(int argc, char **argv ) {
 }
 
 double farmer(int numprocs) {
-  // You must complete this function
+  stack *stack = new_stack();
+  double points[2];
+  double result[3];
+  double *points_p;
+  double total = 0.0;
+  MPI_Status status;
+  int workers = numprocs - 1;
+
+  // 1. Place initial tasks into bag
+  points[0] = A;
+  points[1] = B;
+  push(points, stack);
+
+  while (!is_empty(stack)) {
+    // send all you can
+    while (workers > 0 && !is_empty(stack)) {
+      points_p = pop(stack);
+      MPI_Send(points_p, 2, MPI_DOUBLE, workers, 1, MPI_COMM_WORLD);
+      tasks_per_process[workers] += 1;
+      /*printf("Sending task to %d\n", workers);*/
+      workers--;
+    }
+    while (workers < numprocs - 1) {
+      MPI_Recv(result, 3, MPI_DOUBLE, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+      if (*(result + 1) == -1) {
+        // result was within Epsilon, add to total
+        total += result[0];
+      } else {
+        // add new tasks
+        push(result, stack);
+        push(result + 1, stack);
+      }
+      workers++;
+    }
+  }
+
+  // Terminate all the workers
+  for (workers = numprocs - 1; workers > 0; workers--) {
+    /*printf("%d\n", workers);*/
+    MPI_Send(points_p, 2, MPI_DOUBLE, workers, 0, MPI_COMM_WORLD);
+  }
+
+  return total;
 }
 
 void worker(int mypid) {
-  // You must complete this function
+  double points[2];
+  double result[3];
+  double fleft, fright, mid, fmid, larea, rarea, lrarea;
+  MPI_Status status;
+
+  while(1) {
+    MPI_Recv(points, 2, MPI_DOUBLE, 0, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+    if (0 == status.MPI_TAG) {
+      break;
+    }
+    fleft = F(points[0]);
+    fright = F(points[1]);
+    mid = (points[0] + points[1]) / 2;
+    fmid = F(mid);
+    larea = (fleft + fmid) * (mid - points[0]) / 2;
+    rarea = (fmid + fright) * (points[1] - mid) / 2;
+    lrarea = (fleft + fright) * (points[1] - points[0]) / 2;
+    /*printf("Received %f %f\n", points[0], points[1]);*/
+    usleep(SLEEPTIME);
+    if (fabs((larea + rarea) - lrarea) > EPSILON) {
+      result[0] = points[0];
+      result[1] = mid;
+      result[2] = points[1];
+      MPI_Send(result, 3, MPI_DOUBLE, 0, mypid, MPI_COMM_WORLD);
+    } else {
+      result[0] = larea + rarea;
+      result[1] = -1;
+      result[2] = -1;
+      MPI_Send(result, 3, MPI_DOUBLE, 0, mypid, MPI_COMM_WORLD);
+    }
+  }
+  /*printf("worker %d is done\n", mypid);*/
 }
